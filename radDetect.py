c95d76adcfe33e955ad0883db289303a7fae4740
zabl-en87 <zabl_en87@berkeley.edu>
2026-04-01 17:01:40 -0700

    Update radDetect.py

--- radDetect.py
+++ radDetect.py
@@ -71,7 +71,7 @@ with open(dataName, 'w', newline = '') as csvfile:
 
                 
                         spectra.append(spec_data)
-                        spamwriter.writerow([read_end - read_start, status.total_count])
+                        spamwriter.writerow([read_end - read_start, spec_total])
                         read_times.append(elapsed)
                         reads += 1
 
